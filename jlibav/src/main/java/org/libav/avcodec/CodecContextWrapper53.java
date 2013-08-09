ff115c41673c7d2014f1dfe393f44b060c8dc46b
Ondrej Perutka <perutka.ondrej@gmail.com>
2013-08-09 12:40:46 +0200

    fixed: no packet timestamps when using old avcodec_encode_video()

--- jlibav/src/main/java/org/libav/avcodec/CodecContextWrapper53.java
+++ jlibav/src/main/java/org/libav/avcodec/CodecContextWrapper53.java
@@ -586,6 +586,11 @@ public class CodecContextWrapper53 extends AbstractCodecContextWrapper {
         else if (size == 0)
             return false;
         
+        rebindCodedFrame();
+        IFrameWrapper cf = getCodedFrame();
+        cf.clearWrapperCache();
+        
+        packet.setPts(cf.getPts());
         packet.setSize(size);
         
         return true;
